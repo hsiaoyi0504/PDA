14c39e0a4df7aa27b033015851db0903e85453e7
hsiaoyi <hsiaoyi0504@gmail.com>
2015-05-26 20:46:22 +0800

    update page layout search function complete

--- src/main.js
+++ src/main.js
@@ -1,20 +1,32 @@
 var spotifyApi = new SpotifyWebApi();
 spotifyApi.setPromiseImplementation(Q);
 document.getElementById("search-button").addEventListener("click", search);
+document.getElementById("send-button").addEventListener("click", send);
+document.getElementById("clear-button").addEventListener("click", clear);
 
 function search() {
   var q = $('#query').val();
   // search tracks whose name, album or artist contains 'Love'
-  spotifyApi.searchTracks(q,{limit: 5})
+  const tracksMaxNum=5;
+  spotifyApi.searchTracks('name:'+q,{limit: tracksMaxNum})
   .then(function(data) {
-    console.log('Search by'+q, data);
+    console.log('Search by ',data);
+    for(i=0;i<tracksMaxNum;i++){
+      console.log(i);
+      document.getElementById("track"+i).innerHTML=
+      '<pre>'+'ID:'+data.tracks.items[i].id+'\n'
+             +'Album:'+data.tracks.items[i].album.name+'\n'
+             +'Track:'+data.tracks.items[i].name+'\n'+'</pre>'+'<iframe src="https://embed.spotify.com/?uri=spotify:track:'+data.tracks.items[i].id+'"'+'width="250" height="80" frameborder="0" allowtransparency="true"></iframe>';
+      }
   }, function(err) {
     console.error(err);
   });
-/*
-  request.execute(function(response) {
-    var str = JSON.stringify(response.result);
-    $('#search-container').html('<pre>' + str + '</pre>');
-  });
-*/
+}
+
+function send() {
+
+}
+
+function clear() {
+  
 }
