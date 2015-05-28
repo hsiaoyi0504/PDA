5627619736c8ccac173add977605bd7bca115ea1
hsiaoyi <hsiaoyi0504@gmail.com>
2015-05-29 01:01:57 +0800

    test add other modify database function

--- htdocs/action.php
+++ htdocs/action.php
@@ -3,7 +3,13 @@
 	if($_POST['action'] == 'add') {
 		add();
 	}
+	if($_POST['action'] == 'delete') {
+		delete();
+	}
 	function add(){
 		echo "add";
+	}
+	function delete(){
+		echo "delete";
 	}
 ?>
