16d6d9012aab225e9002fbcdf9e816348f490ec7
hsiaoyi <a26754028@gmail.com>
2015-06-12 12:09:26 +0800

    add http get request processing

--- action.php
+++ action.php
@@ -3,13 +3,11 @@
 	if($_POST['action'] == 'add') {
 		add();
 	}
-	if($_POST['action'] == 'delete') {
-		delete();
+	if($_GET['action'] == 'get') {
+		getQuestion();
 	}
 	function add(){
-		//header("Access-Control-Allow-Origin: *");
 		include("connMysql.php");
-//		if (!@mysql_select_db("class")) die("資料庫選擇失敗！");
 		$sql_query = "INSERT INTO `main` (`questionDescription` ,`trackID`,`choice0` ,`choice1` ,`choice2` ,`choice3` ,`correctChoice`) VALUES (";
 		$sql_query .= "'".$_POST["questionDescription"]."',";
 		$sql_query .= "'".$_POST["trackID"]."',";
@@ -18,9 +16,9 @@
 		$sql_query .= "'".$_POST["choice2"]."',";
 		$sql_query .= "'".$_POST["choice3"]."',";
 		$sql_query .= "'".$_POST["correctChoice"]."')";
-		echo $_POST["questionDescription"];
-		echo $_POST["trackID"];
-		echo $sql_query;
+//		echo $_POST["questionDescription"];
+//		echo $_POST["trackID"];
+//		echo $sql_query;
 		$result=mysqli_query($link,$sql_query);
 		//重新導向回到主畫面
 		//header("Location: data.php");	
@@ -28,13 +26,18 @@
 			echo "false";
 			printf("Errormessage: %s\n", mysqli_error($link));
 		}
-	//	else
-	//		echo "true";
-		//echo "add";
-		//echo $_POST['question'];
 	}
-	function delete(){
-		echo "delete";
+	function getQuestion(){
+		include("connMysql.php");
+		header('Content-Type:application/json; charset=utf-8');
+		$sql_query="SELECT * FROM `main` WHERE `questionID`="."'".$_GET["questionID"]."'";
+		$result=mysqli_query($link,$sql_query);
+		$questionArray=array();
+		while($row=mysqli_fetch_assoc($result)){
+			$questionArray=$row;
+		}
+		$json=json_encode($questionArray);
+		echo $json;
 	}
 ?>
 
