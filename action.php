88b299d07eddc01edc5da43412d2e5af9ac88972
hsiaoyi <a26754028@gmail.com>
2015-06-29 19:58:27 +0800

    add json's structure:songType add background image

--- action.php
+++ action.php
@@ -11,7 +11,26 @@
 	}
 	function add(){
 		include("connMysql.php");
-		$sql_query = "INSERT INTO `english_fashion` (`questionDescription` ,`preview_url`,`choice0` ,`choice1` ,`choice2` ,`choice3` ,`correctChoice`) VALUES (";
+		switch($_POST["songType"]){
+			case "1":
+				$sql_dataTable="english_fashion";
+			break;
+			case "2":
+				$sql_dataTable="english_oldies";
+			break;
+			case "3":
+				$sql_dataTable="chineese_fashion";
+			break;
+			case "4":
+				$sql_dataTable="chineese_oldies";
+			break;
+			case "5":
+				$sql_dataTable="classical";
+			break;
+			default:
+				$sql_dataTable="english_fashion";
+		}
+		$sql_query = "INSERT INTO ".$sgl_dataTable." (`questionDescription` ,`preview_url`,`choice0` ,`choice1` ,`choice2` ,`choice3` ,`correctChoice`) VALUES (";
 		$sql_query .= "'".$_POST["questionDescription"]."',";
 		$sql_query .= "'".$_POST["preview_url"]."',";
 		$sql_query .= "'".$_POST["choice0"]."',";
@@ -19,9 +38,6 @@
 		$sql_query .= "'".$_POST["choice2"]."',";
 		$sql_query .= "'".$_POST["choice3"]."',";
 		$sql_query .= "'".$_POST["correctChoice"]."')";
-//		echo $_POST["questionDescription"];
-//		echo $_POST["trackID"];
-//		echo $sql_query;
 		$result=mysqli_query($link,$sql_query);
 		//重新導向回到主畫面
 		//header("Location: data.php");	
@@ -33,22 +49,59 @@
 	function getQuestion(){
 		include("connMysql.php");
 		header('Content-Type:application/json; charset=utf-8');
-		$sql_query="SELECT * FROM `english_fashion` WHERE `questionID`="."'".$_GET["questionID"]."'";
+		switch($_GET["songType"]){
+			case "1":
+				$sql_dataTable="english_fashion";
+			break;
+			case "2":
+				$sql_dataTable="english_oldies";
+			break;
+			case "3":
+				$sql_dataTable="chineese_fashion";
+			break;
+			case "4":
+				$sql_dataTable="chineese_oldies";
+			break;
+			case "5":
+				$sql_dataTable="classical";
+			break;
+			default:
+				$sql_dataTable="english_fashion";
+		}
+		$sql_query="SELECT * FROM ".$sql_dataTable." WHERE `questionID`="."'".$_GET["questionID"]."'";
 		$result=mysqli_query($link,$sql_query);
 		$questionArray=array();
 		while($row=mysqli_fetch_assoc($result)){
 			$questionArray=$row;
 		}
-//		print_r($questionArray);
 		$json=json_encode($questionArray);
 		echo $json;
 	}
 	function get3Question(){
 		include("connMysql.php");
 		header('Content-Type:application/json; charset=utf-8');
-		$sql_query1="SELECT * FROM `english_fashion` WHERE `questionID`="."'".$_GET["questionID1"]."'";
-		$sql_query2="SELECT * FROM `english_fashion` WHERE `questionID`="."'".$_GET["questionID2"]."'";
-		$sql_query3="SELECT * FROM `english_fashion` WHERE `questionID`="."'".$_GET["questionID3"]."'";
+		switch($_POST["songType"]){
+			case "1":
+				$sql_dataTable="english_fashion";
+			break;
+			case "2":
+				$sql_dataTable="english_oldies";
+			break;
+			case "3":
+				$sql_dataTable="chineese_fashion";
+			break;
+			case "4":
+				$sql_dataTable="chineese_oldies";
+			break;
+			case "5":
+				$sql_dataTable="classical";
+			break;
+			default:
+				$sql_dataTable="english_fashion";
+		}
+		$sql_query1="SELECT * FROM ".$sql_dataTable." WHERE `questionID`="."'".$_GET["questionID1"]."'";
+		$sql_query2="SELECT * FROM ".$sql_dataTable." WHERE `questionID`="."'".$_GET["questionID2"]."'";
+		$sql_query3="SELECT * FROM ".$sql_dataTable." WHERE `questionID`="."'".$_GET["questionID3"]."'";
 		$result1=mysqli_query($link,$sql_query1);
 		$result2=mysqli_query($link,$sql_query2);
 		$result3=mysqli_query($link,$sql_query3);
